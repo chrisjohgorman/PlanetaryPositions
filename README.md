dd8907efe3abefc90d84ee25245a391583454b28
Chris Gorman <chrisjohgorman@gmail.com>
2018-05-06 17:19:46 -0400

    Missed comitting changes to README.md and PlanetaryPositions.jl because of missing -a to git commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
+
 # PlanetaryPositions
+
+*Port of Octave scripts to determine planetary positions* 
+
+## About
+
+PlanetaryPositions is a Julia package to perform calculations of the positions 
+of the planets in our solar system.  I use the calculations from 
+Paul Schlyter, Stockholm, Sweden and his tutorial available at 
+http://stjarnhimlen.se/ .
+
+These files constitute an effort to understand how to build a package for 
+Julia and to use it for software development.  I am moving files from a set
+of Octave scripts which allow for calculations of the positions of the 
+planets.  These are a poorly done hack and I will be spending my time making
+them look better and converting the syntax from .m files to .jl.
+
--- src/PlanetaryPositions.jl
+++ src/PlanetaryPositions.jl
@@ -1,5 +1,27 @@
 module PlanetaryPositions
 
-# package code goes here
+latitude = 45.480295
+longitude = -75.466788
+
+day = day_number(1998,8,10)
+utc_time = 0
+
+su = sun(day,latitude,longitude,utc_time)
+me = mercury(day)
+ve = venus(day)
+moon_az_alt = moon(day,latitude,su(3))
+ma = mars(day)
+ju = jupiter(day)
+sa = saturn(day)
+ur = uranus(day)
+ne = neptune(day)
+
+mercury_az_alt = az_alt(su(3),me(4),me(5),latitude)
+venus_az_alt = az_alt(su(3),ve(4),ve(5),latitude)
+mars_az_alt = az_alt(su(3),ma(4),ma(5),latitude)
+jupiter_az_alt = az_alt(su(3),ju(4),ju(5),latitude)
+saturn_az_alt = az_alt(su(3),sa(4),sa(5),latitude)
+uranus_az_alt = az_alt(su(3),ur(4),ur(5),latitude)
+neptune_az_alt = az_alt(su(3),ne(4),ne(5),latitude)
 
 end # module
