714aff1f481a297dd39fbd5636e32ed32e62fa48
Chris Gorman <chrisjohgorman@gmail.com>
2018-05-11 15:32:10 -0400

    Brought back old variable names for mars.jl

--- src/mars.jl
+++ src/mars.jl
@@ -1,49 +1,41 @@
-
 function mars(day_number)
-    	node =  49.5574 + 2.11081e-5 * day_number
-    	inclination = 1.8497 - 1.78e-8 * day_number
-    	argument_of_perihelion = 286.5016 + 2.92961e-5 * day_number
-    	axis = 1.523688	
-    	eccentricity = 0.093405 + 2.516e-9 * day_number
-    	mean_anomaly =  18.6021 + 0.5240207766 * day_number
-	mean_anomaly = revolve(mean_anomaly)
-        obliquity_of_ecliptic = 23.4393 - 3.563e-7 * day_number 
+    	N =  49.5574 + 2.11081e-5   * day_number
+    	i =   1.8497 - 1.78e-8      * day_number
+    	w = 286.5016 + 2.92961e-5   * day_number
+    	a = 1.523688				
+    	e = 0.093405     + 2.516e-9 * day_number
+    	M =  18.6021 + 0.5240207766 * day_number
+	M = revolve(M)
+        oblecl = 23.4393 - 3.563e-7 * day_number # obliquity of the eliptic
 
-	EccentricAnomaly = eccentric_anomaly(mean_anomaly, eccentricity, 0.0005)
+	E = eccentric_anomaly(M, e, 0.0005)
         # mars's rectrangular coordinates
-        x = axis * (cosd(EccentricAnomaly) - eccentricity)
-        y = axis * sind(EccentricAnomaly) * sqrt(1 - eccentricity^2)
+        x = a * (cosd(E) - e)
+        y = a * sind(E) * sqrt(1 - e*e)
         # convert to distance and true anomaly
-        distance = sqrt(x*x + y*y)
-        true_anomaly = atan2(y, x) * (180/π)
+        r = sqrt(x*x + y*y)
+        v = atan2(y, x) * (180/π)
         # mars's position in ecliptic coordinates
-        x_ecliptic = distance * ( cosd(node) 
-		* cosd(true_anomaly + argument_of_perihelion) 
-		- sind(node) * sind(true_anomaly + argument_of_perihelion) 
-		* cosd(inclination))
-        y_ecliptic = distance * ( sind(node) 
-		* cosd(true_anomaly + argument_of_perihelion) 
-		+ cosd(node) * sind(true_anomaly + argument_of_perihelion) 
-		* cosd(inclination))
-        z_ecliptic = distance * (sind(true_anomaly + argument_of_perihelion)
-		* sind(inclination))
+        xeclip = r * ( cosd(N) * cosd(v+w) - sind(N) * sind(v+w) * cosd(i))
+        yeclip = r * ( sind(N) * cosd(v+w) + cosd(N) * sind(v+w) * cosd(i))
+        zeclip = r * sind(v+w) * sind(i)
         # add sun's rectangular coordinates
-        SunRectangular = sun_rectangular(day_number)
-        x_geocentric = SunRectangular[1] + x_ecliptic
-        y_geocentric = SunRectangular[2] + y_ecliptic
-        z_geocentric = SunRectangular[3] + z_ecliptic
+        sunr = sun_rectangular(day_number)
+        xgeoc = sunr[1] + xeclip
+        ygeoc = sunr[2] + yeclip
+        zgeoc = sunr[3] + zeclip
         # rotate the equitorial coordinates
-        x_equatorial = x_geocentric
-        y_equatorial = y_geocentric * cosd(obliquity_of_ecliptic) - z_geocentric * sind(obliquity_of_ecliptic)
-        z_equatorial = y_geocentric * sind(obliquity_of_ecliptic) + z_geocentric * cosd(obliquity_of_ecliptic)
-        # convert to right_ascesion and Decl
-        right_ascesion = atan2(y_equatorial, x_equatorial) * (180/π)
-        right_ascesion = revolve(right_ascesion)
-	right_ascesion = right_ascesion / 15
-        declination = atan2(z_equatorial, sqrt(x_equatorial^2 + y_equatorial^2)) * (180/π)
+        xequat = xgeoc
+        yequat = ygeoc * cosd(oblecl) - zgeoc * sind(oblecl)
+        zequat = ygeoc * sind(oblecl) + zgeoc * cosd(oblecl)
+        # convert to RA and Decl
+        RA = atan2(yequat, xequat) * (180/π)
+        RA = revolve(RA)
+	RA = RA / 15
+        Decl = atan2(zequat, sqrt(xequat*xequat + yequat*yequat)) * (180/π)
         # convert to ecliptic longitude and latitude
-        longitude = atan2(y_ecliptic, x_ecliptic) * (180/π)
-        longitude = revolve(longitude)
-        latitude = atan2(z_ecliptic, sqrt(x_ecliptic^2 + y_ecliptic^2)) *(180/π)
-	return [longitude, latitude, distance, right_ascesion, declination]
+        lon = atan2(yeclip, xeclip) * (180/π)
+        lon = revolve(lon)
+        lat = atan2(zeclip, sqrt(xeclip*xeclip + yeclip*yeclip)) * (180/π)
+	return [lon, lat, r, RA, Decl]
 end
